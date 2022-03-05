df11f13a16a6e331d6aa45795f28655fc2abcdac
VictorBlay <victorblay3@yahoo.es>
2022-03-05 21:54:37 +0100

    ApiMid

--- api-bcn/main.py
+++ api-bcn/main.py
@@ -1,3 +1,24 @@
 from fastapi import FastAPI
+from routers import persona
 
 app = FastAPI()
+app.include_router(persona.router)
+
+
+@app.get("/")
+async def root():
+    return {"message":"Hello"}
+
+#lanzar la API en la terminal: uvicorn main:app
+#lanzar y que se puede modificar: uvicorn main:app --reload
+#primero los endpoints fijos y después parametrizados por niveles
+
+@app.post("/adios")
+async def root():
+    return {"message":"Adios Hello"}
+
+
+#web params
+@app.get("/suma")
+async def suma(a:int, b:int):
+    return {"result":a+b}
---
+++ api-bcn/routers/persona.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/persona/{name}")
+async def person_name(name):
+    return {"message":f"Endpoint parametrizado, {name}"}
+
+@router.get("/persona/{name}/{age}")
+async def person_name(name:str, age:int):
+    return {
+        "name":{
+            "value":name,
+            "type":str(type(name))
+        },
+        "age":{
+            "value":age,
+            "type":str(type(age))
+        }
+    }
