d44b15bb5232adce58d7ef3f1f93f54a29e49696
VictorBlay <victorblay3@yahoo.es>
2022-03-06 13:59:01 +0100

    cambios

---
+++ api/api-bcn/database/mongo.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+username = os.getenv("MONGO_USER")
+password = os.getenv("MONGO_PASS")
+url = f"mongodb+srv://{username}:{password}@midprojectcore.ot3vw.mongodb.net/test"
+
+db = MongoClient(url).get_database("midprojectcore")
+
+
