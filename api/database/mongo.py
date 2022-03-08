d9650953fc4af21563a39ec8abe122699012538b
VictorBlay <victorblay3@yahoo.es>
2022-03-08 23:56:56 +0100

    cambios

--- api/database/mongo.py
+++ api/database/mongo.py
@@ -10,4 +10,8 @@ url = f"mongodb+srv://{username}:{password}@databcn.ot3vw.mongodb.net/test"
 
 db = MongoClient(url).get_database("databcn")
 
+def get_data(collection, filter={}, project={"_id":0}, limit=0, skip=0):
+    return list(db[collection].find(filter,project).limit(limit).skip(skip))
 
+def distinct(collection, field):
+    return db[collection].distinct(field)
--- api/routers/poblacion.py
+++ api/routers/poblacion.py
@@ -1,26 +1,28 @@
 import json
 from fastapi import APIRouter
-from database.mongo import db
+from database.mongo import get_data, distinct
 from bson import json_util
 from json import loads
-from error.error import error
 
 router = APIRouter()
 
 @router.get("/Population/DataGender/{Gender}")
-@error
 async def genero(Gender):
-    res = list(db["poblacion"].find({"Gender":Gender}))
+    res = get_data("poblacion", {"Gender":Gender}, {'Age':1,'Number':1,'_id':0})
     return loads(json_util.dumps(res))
 
-@router.get("/Population/DataGender/{Gender}")
-@error
-async def genero(Gender):
-    res = list(db["poblacion"].find({"Gender":Gender}))
+
+@router.get("/TotalPopulation/Year/{Year}")
+async def poblacion_total(Year):
+    res = get_data("poblacion", {"Year":int(Year)}, {'Number':1,'_id':0})
+    total_pop = [tot["Number"] for tot in res]
+    suma = sum(total_pop)
+    return loads(json_util.dumps(suma))
+
+
+@router.get("/Population/Neighborhood")
+async def genero():
+    res = distinct("poblacion", "Neighborhood.Name")
     return loads(json_util.dumps(res))
 
-@router.get("/Population/DataGender/{Gender}")
-@error
-async def genero(Gender):
-    res = list(db["poblacion"].find({"Gender":Gender}))
-    return loads(json_util.dumps(res))
+
